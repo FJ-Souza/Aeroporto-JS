46bffc66c23d8c756b61f45a6e752ccc54f931cb
RaphaZY <raphacoelgo3790@gmail.com>
2024-05-27 15:05:03 -0300

    aero5

--- aeroporto01/js/scripts.js
+++ aeroporto01/js/scripts.js
@@ -3,34 +3,73 @@ function inicializa(){
     bt.onclick = consultaClima;
 }
 
-function consultaClima(){
-    var clima = document.getElementById('clima');
+function consultaClima() {
+    var clima = document.getElementById('clima').value;
+    var nomeEstacao = '';
+    switch (clima) {
+        case 'SBAR':
+            nomeEstacao = 'Santa Maria - SE';
+            break;
+        case 'SBGR':
+            nomeEstacao = 'Guarulhos - SP';
+            break;
+        case 'SBUL':
+            nomeEstacao = 'Uberlândia - MG';
+            break;
+        case 'SBCB':
+            nomeEstacao = 'Cabo Frio - RJ';
+            break;
+        default:
+            nomeEstacao = 'Estação Desconhecida';
+            break;
+    }
+    
     var div = document.getElementById('resultado');
-    var url = 'http://servicos.cptec.inpe.br/XML/estacao/' + clima.value + '/condicoesAtuais.xml/';
+    var url = 'http://servicos.cptec.inpe.br/XML/estacao/' + clima + '/condicoesAtuais.xml';
     console.log(url);
 
- 
-    xmlhttp = new XMLHttpRequest();
-    
-    //abre o arquivo xml utilizando o metodo get
-    xmlhttp.open("GET",url,true);
-    // envia o conteudo
-    xmlhttp.send();
-    // especifica o tipo de retorno
-    xmlDoc = xmlhttp.responseXML;
-    //cria o objeto x que recebe os objetos metar do XML
-    xmlDoc.onerror = function(e){
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onerror = function(){
         div.innerHTML = 'VALOR INVÁLIDO!';
     }
 
-    xmlDoc.onload =()=>{
-        var response = responseXML(xmlDoc.responseText);
-        if(response.erro === true){
-            div.innerHTML = 'CEP NÃO ENCONTRADO!';
+
+    xmlhttp.onload = function() {
+        if (xmlhttp.status === 200) {
+            var xmlDoc = xmlhttp.responseXML;
+            if (xmlDoc) {
+                var metar = xmlDoc.getElementsByTagName("metar")[0];
+                if (metar) {
+                    var codigo = metar.getElementsByTagName("codigo")[0].textContent;
+                    var temperatura = metar.getElementsByTagName("temperatura")[0].textContent;
+                    var umidade = metar.getElementsByTagName("umidade")[0].textContent;
+                    var pressao = metar.getElementsByTagName("pressao")[0].textContent;
+
+                    div.innerHTML = `
+                        Código: ${codigo}<br>
+                        Aeroporto: ${nomeEstacao}<br>
+                        Temperatura: ${temperatura}°C<br>
+                        Umidade: ${umidade}%<br>
+                        Pressão: ${pressao} hPa
+                    `;
+                } else {
+                    div.innerHTML = 'Dados não disponíveis para a estação.';
+                }
+            } else {
+                div.innerHTML = 'Erro ao processar a resposta XML.';
+            }
         } else {
-            let x = xmlDoc.getElementsByTagName("metar");
-            div.innerHTML(x.getElementsByTagName("codigo").childNodes.nodeValue);    
+            div.innerHTML = 'Erro ao realizar a requisição: ' + xmlhttp.status;
         }
-    }
-    xmlDoc.send();
+    };
+
+  
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
 }
